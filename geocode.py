4fd8c1724a25eb0f9f34c936a6660a0e31536bce
David Kaplita <kaplitde@gmail.com>
2019-03-27 20:00:37 -0400

    Initial commit of basic REST server and GeoCode
    geocode.py - Contains a basic function use Google Cloud Platform's
      GeoCode.  This will eventually be integrated into the web server
    
    webserver.py - Basic REST server returning data

---
+++ geocode.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import httplib2
+import json
+import requests
+
+def getGeocodeLocation(inputString):
+    # Use Google Maps to convert a location into Latitute/Longitute coordinates
+    # FORMAT: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY
+    google_api_key = "AIzaSyDa8_XuZQW0caJagxp83JD_wHb1kxYqvVU"
+    locationString = inputString.replace(" ", "+")
+    url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
+    #h = httplib2.Http()
+    print(url)
+    #reply_str = h.request(url,'GET')[1]
+    #print(reply_str)
+    #result = json.loads(str(reply_str))
+    result = requests.get(url).json()
+    latitude = result['results'][0]['geometry']['location']['lat']
+    longitude = result['results'][0]['geometry']['location']['lng']
+    return (latitude,longitude)
+
+getGeocodeLocation("Tokyo, Japan")
+
+print(getGeocodeLocation("Tokyo Japan"))
+print(getGeocodeLocation("Jakarta Indonesia"))
+print(getGeocodeLocation("Maputo Mozambique"))
+print(getGeocodeLocation("Geneva Switzerland"))
+print(getGeocodeLocation("Los Angeles California USA"))

da623025059b11fe7c2acacc382be6cc88d2f3f6
David Kaplita <48358450+DavidKaplita@users.noreply.github.com>
2019-03-27 20:03:48 -0400

    GeoCode Remove Activation Key
    Remove the hard coded Application Key.  The activation key
    for the Google account will also be removed.

--- geocode.py
+++ geocode.py
@@ -7,7 +7,7 @@ import requests
 def getGeocodeLocation(inputString):
     # Use Google Maps to convert a location into Latitute/Longitute coordinates
     # FORMAT: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY
-    google_api_key = "AIzaSyDa8_XuZQW0caJagxp83JD_wHb1kxYqvVU"
+    google_api_key = ""
     locationString = inputString.replace(" ", "+")
     url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
     #h = httplib2.Http()
